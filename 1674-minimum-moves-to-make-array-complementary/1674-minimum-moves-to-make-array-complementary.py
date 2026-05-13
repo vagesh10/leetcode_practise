c6ab0968b1446f6506f7fcf9b95730167fd0d6e9
vagesh10 <vagee.surla4@gmail.com>
2026-05-13 20:58:50 +0530

    Time: 212 ms (50.77%), Space: 31.1 MB (60%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
@@ -1,31 +1,21 @@
 class Solution:
     def minMoves(self, nums: List[int], limit: int) -> int:
         
-        n=len(nums)
+        
 
-        diff=[0] * (2*limit+2)
-
-        for i in range(n//2):
-            a=nums[i]
-            b=nums[n-1-i]
-
-
-            low=1+min(a,b)
-            high=limit+max(a,b)
-            pair_sum=a+b 
-
-            diff[2]+=2
-            diff[2*limit+1]-=2
-
-            diff[low]-=1
-            diff[high+1]+=1
-
-            diff[pair_sum]-=1
-            diff[pair_sum+1]+=1
-        ans=float('inf')
-        cur=0
-        for s in range(2,2*limit+1):
-            cur+=diff[s]
-            ans=min(ans,cur)
-
-        return ans
+        n = len(nums)
+        dp = [0] * (2 * limit + 2)
+        for i in range(n // 2):
+            mini = min(nums[i], nums[-1 - i])
+            maxi = max(nums[i], nums[-1 - i])
+            dp[2] += 2
+            dp[mini + 1] -= 1
+            dp[mini + maxi] -= 1
+            dp[mini + maxi + 1] += 1
+            dp[maxi + limit + 1] += 1
+        res = n
+        moves = 0
+        for c in range(2, 2 * limit + 1):
+            moves += dp[c]
+            res = min(res, moves)
+        return res
