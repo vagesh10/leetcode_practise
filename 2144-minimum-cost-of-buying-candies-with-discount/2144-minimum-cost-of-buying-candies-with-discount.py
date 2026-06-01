35ee087148fcfd07adc7558df99778bbc6447519
vagesh10 <vagee.surla4@gmail.com>
2026-06-01 09:41:38 +0530

    Time: 3 ms (27.74%), Space: 19.4 MB (15.37%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -4,16 +4,16 @@ class Solution:
         n=len(cost)
         c=sorted(cost,reverse=True)
         s=0
-        if n==1:
-            return c[0]
-        if (n==2):
-            return c[0]+c[1]
+        # if n==1:
+        #     return c[0]
+        # if (n==2):
+        #     return c[0]+c[1]
         
-        else:
-            for i in range(n):
-                if(i%3!=2):
-                    print(c[i])
-                    s+=c[i]
+    
+        for i in range(n):
+            if(i%3!=2):
+                print(c[i])
+                s+=c[i]
         return s
             
 
