2939f318fd66ada2cb28ee77f75f64bff402dc0c
vagesh10 <vagee.surla4@gmail.com>
2026-06-07 10:01:04 +0530

    Time: 0 ms (100%), Space: 19.2 MB (74.74%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,18 +2,17 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
         prior=[1]*n
-        cur=[0]*n
-
-        cur[0]=1 
+       
 
         for i in range(1,m):
+            cur=[1]*n
             for j in range(1,n):
                 cur[j]=cur[j-1]+prior[j]
             
+            prior=cur
+            
 
-            for k in range(1,n):
-                prior[k]=cur[k]
-        
+           
 
         return prior[n-1]
         
