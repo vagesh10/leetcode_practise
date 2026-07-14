6d21399d3db9dbef87b76617935927722ce1684b
vagesh10 <vagee.surla4@gmail.com>
2026-07-14 21:52:58 +0530

    Time: 73 ms (11.11%), Space: 28.9 MB (100%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+
+
+        s=sorted(arr)
+        unique=[]
+        for x in s:
+            if not unique or unique[-1] != x:
+                unique.append(x)
+        for i in range(len(arr)):
+            arr[i]=bisect_left(unique,arr[i])+1
+        
+
+        return arr
+
+
+
+
+        
